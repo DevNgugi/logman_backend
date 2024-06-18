867d9d77f2c7a01da6fe76d19170a79aa49de37d
pngugi <pngugi@mail.com>
2024-06-18 16:14:08 +0300

    todo: remove double messages

--- api/services/streamer.py
+++ api/services/streamer.py
@@ -1,54 +1,33 @@
+import json
+import asyncio
+from channels.generic.websocket import AsyncWebsocketConsumer
 import paramiko
-import time
 
-class SSHConnection:
-    def __init__(self):
-        self.ssh = paramiko.SSHClient()
-        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.log_lines = []
-        self.hostname = '165.90.23.196'
-        self.port = 8044
-        self.username = 'pngugi'
-        self.password = 'C0nn4cT140'
-        self.read_log()
+async def send_message_every_second():
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        log_lines = []
 
-
-    def connect(self):
+        hostname = '165.90.23.196'
+        port = 8044
+        username = 'pngugi'
+        password = 'C0nn4cT140'
+        command = ('tail -f /var/log/county/ug_county.out.log')
+        ssh.connect(hostname, port=port, username=username, password=password)
+        stdin, stdout, stderr = ssh.exec_command(command, get_pty=True)
         try:
-            self.ssh.connect(self.hostname, port=self.port, username=self.username, password=self.password)
-            return True
-        except Exception as e:
-            print(f"An error occurred while connecting: {e}")
-            return False
-
-    def read_log(self):
-        try:
-            if self.connect():
-                command = ('tail -f /var/log/county/ug_county.out.log')
-                self.close()
-            stdin, stdout, stderr = self.ssh.exec_command(command, get_pty=True)
             while True:
-                line = stdout.readline()
-                if not line:
-                    break
-                self.log_lines.append(line.strip())
-                print(line.strip())
-                time.sleep(0.1)
+                if stdout.channel.recv_ready():
+                    line = stdout.readline()
+                    if line:
+                        message = line.strip()
+                        print(message)
+               
+                await asyncio.sleep(0.1)
+
         except Exception as e:
-            print(f"An error occurred while reading log: {e}")
+                print(f"An error occurred while reading log: {e}")
+                ssh.close()
 
-    def close(self):
-        self.ssh.close()
-
-# Example usage
-
-
-if __name__ == "__main__":
-    hostname = '165.90.23.196'
-    port = 8044
-    username = 'pngugi'
-    password = 'C0nn4cT140'
-    log_reader = SSHLogReader(hostname, port, username, password)
-    if log_reader.connect():
-        log_reader.read_log('tail -f /var/log/county/ug_county.out.log')
-        log_reader.close()
+if __name__ =='__main__':
+     asyncio.run(send_message_every_second())
