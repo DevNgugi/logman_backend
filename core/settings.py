709e093d49804b326e366c7c05c82f4a72b27fc3
pngugi <pngugi@mail.com>
2024-06-19 20:14:53 +0300

    encrypt pass

--- core/settings.py
+++ core/settings.py
@@ -121,8 +121,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),  # Example token expiration time
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),     # Example refresh token expiration time
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30), 
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,
