3a0f2e867e79d4b3907cdcbefd80e3e9a11f8b54
devngugi <philemonngugi18@gmail.com>
2024-06-20 07:47:44 +0300

    default groups

--- api/views.py
+++ api/views.py
@@ -1,5 +1,4 @@
 from .models import Connection, Source
-from rest_framework import viewsets
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import ConnectionSerializer, SourceSerializer
 from rest_framework.permissions import IsAuthenticated
