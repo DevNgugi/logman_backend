8529ebf401fd9c7f60f40ab75d17b5fa16660461
Ngugi Philemon <113933822+DevNgugi@users.noreply.github.com>
2025-03-06 11:20:03 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -132,9 +132,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'noreply@jambopay.com'
-EMAIL_HOST_PASSWORD = 'Trent655!!'
-DEFAULT_FROM_EMAIL = 'noreply@jambopay.com' 
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = '' 
 
 
 TIME_ZONE = 'UTC'
